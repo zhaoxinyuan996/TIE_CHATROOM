5a2ecc72ac74cf88d1e30d320b2a06ae08b3a507
zxy <1178232101@qq.com>
2020-06-06 19:23:44 +0800

    添加日志模块

--- libs/logger.py
+++ libs/logger.py
@@ -20,16 +20,16 @@ class Logger:
         self._p = p
 
     def _to_file(self, info):
-        with open('%s.txt' % strftime('%Y-%m-%d'), 'a') as f:
+        with open('%s%s.txt' % (LoggerSettings.logFilePath, strftime('%Y-%m-%d')), 'a') as f:
             f.write('%s\n' % info)
 
     def _output(self, level, info):
         info = '%s [%s] %s %s -> line.%s:%s' % (
-            strftime('%Y-%m-%d %H:%M:%S'), # 时间
-            self._loggerDict[level][1],    # 级别
-            currentThread(),               # 线程号
-            _getframe(2).f_code.co_filename,           # 所在函数
-            _getframe(2).f_lineno,         # 所在行
+            strftime('%Y-%m-%d %H:%M:%S'),   # 时间
+            self._loggerDict[level][1],      # 级别
+            currentThread(),                 # 线程号
+            _getframe(2).f_code.co_filename, # 所在函数
+            _getframe(2).f_lineno,           # 所在行
             info)
 
         if self._w: self._to_file(info)
