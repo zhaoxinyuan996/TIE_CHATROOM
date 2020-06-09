aa48d5bafb52433134f64293a9de9c9ea89ebabf
wb-zxy445784 <1178232101@qq.com>
2020-06-09 15:51:48 +0800

    聊天室初显成

--- app_chatroom/views.py
+++ app_chatroom/views.py
@@ -19,35 +19,45 @@ def _all_user_send(m: (str, bytes), q: set) -> None:
     if not m: return
     if isinstance(m, str):
         m = m.encode()
+    print('mmmmmmmmmmmm', m, type(m), sessionSet, q)
     if q:
         for i in q:
             i.websocket.send(m)
 
 
+def _join(request):
+    msg = '%s 加入' % request
+    _all_user_send(msg, sessionSet)
+    sessionSet.add(request)
+
+def _speak(msg):
+    if msg:
+        _all_user_send(msg, sessionSet)
+
+def _leave(request):
+    sessionSet.remove(request)
+    msg = '%s 离开' % request
+    _all_user_send(msg, sessionSet)
+
+
 @accept_websocket
-def cli_accept(request) -> HttpResponse:
-    print(type(request))
+def cli_accept(request):
+
     if request.is_websocket():
 
         # 加入
-        msg = '%s 加入' % request
-        _all_user_send(msg, sessionSet)
-        sessionSet.add(request)
+        _join(request)
 
         while True:
             # 异步 阻塞 等待客户端发消息
             msg = request.websocket.wait()
             # 断连
-            print(request.websocket.is_closed())
             if request.websocket.is_closed():
-                sessionSet.remove(request)
-                msg = '%s 离开' % request
-                _all_user_send(msg, sessionSet)
-                return HttpResponse(b'LOSS!')
-
+                _leave(request)
+                return HttpResponse(b'LEAVE')
             # 发言
-            if msg:
-                _all_user_send(msg, sessionSet)
+            _speak(msg)
 
-    return HttpResponse(b'NO!')
+    return HttpResponse(b'EXIT')
+
 
