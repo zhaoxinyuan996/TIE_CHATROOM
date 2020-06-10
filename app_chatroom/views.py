db34b526e9227345ea8e90135ea5791fea68703c
zxy <1178232101@qq.com>
2020-06-10 22:23:03 +0800

    修复小bug

--- app_chatroom/views.py
+++ app_chatroom/views.py
@@ -17,7 +17,7 @@ from django.shortcuts import render
 from dwebsocket.decorators import require_websocket, accept_websocket
 from libs import logger
 from TIE.settings import WordsQueueConf
-from app_chatroom.models import ChatUser
+from app_chatroom.models import ChatUser, SocketPool
 from tools.thesaurus import wordsFilterTool
 
 # TODO:多个聊天室后期实现，大概会做一个初次信息判定，初次判定时 用户资料（昵称和ip）会被整合至request请求
@@ -85,6 +85,7 @@ accept_header = (
             "Upgrade: websocket\r\n"
             "Connection: Upgrade\r\n"
             "Sec-WebSocket-Accept: %s\r\n"
+            "\r\n"
             )
 
 
@@ -133,13 +134,44 @@ def _write_frame(sock, data, fin=True, opcode=0x1):
     except socket.error:
         sock.close()
 
-from threading import Thread
-def rec(s):
-    while True:
-        try:
-            r = s.recv(1024)
-            print('r是', r)
-        except:pass
+def _read_strict(sock, bufsize):
+    remaining = bufsize
+    _bytes = b""
+    while remaining:
+        _buffer = sock.recv(remaining)
+        if not _buffer:
+            raise socket.error(socket.EBADF, 'Bad file descriptor')
+        _bytes += _buffer
+        remaining = bufsize - len(_bytes)
+    return _bytes
+
+def read_frame(sock):
+    """
+    recieve data as frame from server.
+    """
+    header_bytes = _read_strict(sock, 2)
+    b1 = header_bytes[0] if six.PY3 else ord(header_bytes[0])
+    fin = b1 >> 7 & 1
+    opcode = b1 & 0xf
+    b2 = header_bytes[1] if six.PY3 else ord(header_bytes[1])
+    mask = b2 >> 7 & 1
+    length = b2 & 0x7f
+
+    length_data = ""
+    if length == 0x7e:
+        length_data = _read_strict(sock, 2)
+        length = struct.unpack("!H", length_data)[0]
+    elif length == 0x7f:
+        length_data = _read_strict(sock, 8)
+        length = struct.unpack("!Q", length_data)[0]
+    mask_key = ""
+    if mask:
+        mask_key = _read_strict(sock, 4)
+    data = _read_strict(sock, length)
+    if mask:
+        data = mask_or_unmask(mask_key, data)
+    return fin, opcode, data
+
 # @accept_websocket
 def cli_accept(request) -> HttpResponse:
     '''客户端总处理函数'''
@@ -149,35 +181,27 @@ def cli_accept(request) -> HttpResponse:
     k = compute_accept_value(h)
     s = accept_header % k.decode()
 
-    print(s)
+
     if isinstance(s, str): s = s.encode()
-    # t = Thread(target=rec, args=(cliSocket, ))
-    # t.start()
+    print(s)
     cliSocket.send(s)
 
-    # _write_frame(cliSocket, 'this is data'.encode())
     for i in range(5):
         pass
         print('send')
-        _write_frame(cliSocket, 'this is data'.encode())
-        # break
+        _write_frame(cliSocket, b'{"message": "123", "type": "usermsg", "name": "123"}')
+    mySock = SocketPool(cliSocket)
+    while not mySock.can_read():
+        _, _, res = read_frame(cliSocket)
+        res = json.loads(res.decode())
+        res['type'] = 'usermsg'
+        res = json.dumps(res).encode()
+        for i in l:
+            print('遍历', i)
+            _write_frame(i, res)
 
 
 
-    # if request.is_websocket():
-    #
-    #
-    #     _join(request)
-    #
-    #     while True:
-    #         msg = request.websocket.wait()
-    #
-    #         if request.websocket.is_closed():
-    #             _leave(request)
-    #             return HttpResponse(b'CLIENT LEAVE')
-    #
-    #         _speak(msg, request)
-    #
-    return HttpResponse(b'FORCED EXIT')
 
 
+    return HttpResponse(b'OK')
