76df62c6d41b68ead481b932872a1601b4d0d96c
wb-zxy445784 <1178232101@qq.com>
2020-06-11 10:47:19 +0800

    自建类

--- app_chatroom/models.py
+++ app_chatroom/models.py
@@ -1,22 +1,44 @@
+import json
 import array
+import base64
+import hashlib
+import os
 import select
 import struct
 
 import six
 import socket
 
+from libs import myLog
+from tools.thesaurus import wordsFilterTool
 from TIE.settings import ChatUserConf, WordsQueueConf
 
-# 用户类
+
+accept_header = (
+            "HTTP/1.1 101 Switching Protocols\r\n"
+            "Upgrade: websocket\r\n"
+            "Connection: Upgrade\r\n"
+            "Sec-WebSocket-Accept: %s\r\n"
+            "\r\n"
+            )
+
+# 自定义，声明断连
+class ShortMsgError(Exception): pass
+
 class ChatUser():
-
-    __slots__ = ('ip', 'level', 'speakTimes', '_levelTable')
-
-    def __init__(self, ip: str) -> None:
-        self.ip = ip
+    def __init__(self, request):
+        if 'HTTP_X_FORWARDED_FOR' in request.META:
+            self.ip = request.META['HTTP_X_FORWARDED_FOR']
+        else:
+            self.ip = request.META['REMOTE_ADDR']
         self.level = 0
         self.speakTimes = 0
         self._levelTable = ChatUserConf.levelTable_speak
+
+        self.request = request
+        self.sock = self._get_wsgi_sock(request)
+
+        self.handshake()
 
     def __getattr__(self, item: str) -> None:
         return None
@@ -29,32 +51,50 @@ class ChatUser():
 
         return words, None
 
-class SocketPool():
-    def __init__(self, sock):
-        self.sock = sock
-        self.pool = {}
+    def handshake(self) -> None:
+        k = self._compute_accept_value(self.request.META['HTTP_SEC_WEBSOCKET_KEY'])
+        s = accept_header % k.decode()
+        self.sock.send(s.encode())
 
-    @classmethod
-    def mask_or_unmask(cls, mask, data):
-        """Websocket masking function.
-        `mask` is a `bytes` object of length 4; `data` is a `bytes` object of any length.
-        Returns a `bytes` object of the same length as `data` with the mask applied
-        as specified in section 5.3 of RFC 6455.
-        This pure-python implementation may be replaced by an optimized version when available.
-        """
+    @staticmethod
+    def _compute_accept_value(key: str) -> bytes:
+        sha1 = hashlib.sha1()
+        sha1.update(key.encode())
+        sha1.update(b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11")  # Magic value
+        return base64.b64encode(sha1.digest())
+
+    def _get_wsgi_sock(self, request) -> socket:
+        if 'gunicorn.socket' in request.META:
+            sock = request.META['gunicorn.socket']
+        else:
+            wsgi_input = request.META['wsgi.input']
+            if hasattr(wsgi_input, '_sock'):
+                sock = wsgi_input._sock
+            elif hasattr(wsgi_input, 'rfile'):  # gevent
+                if hasattr(wsgi_input.rfile, '_sock'):
+                    sock = wsgi_input.rfile._sock
+                else:
+                    sock = wsgi_input.rfile.raw._sock
+            elif hasattr(wsgi_input, 'raw'):
+                sock = wsgi_input.raw._sock
+            elif hasattr(wsgi_input, 'stream') and hasattr(wsgi_input.stream, 'raw'):
+                sock = wsgi_input.stream.raw._sock
+            else:
+                raise ValueError('Socket not found in wsgi.input')
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return sock
+    @staticmethod
+    def _mask_or_unmask(mask: bytes, data: bytes) -> (str, bytes):
         mask = array.array("B", mask)
         unmasked = array.array("B", data)
         for i in range(len(data)):
             unmasked[i] = unmasked[i] ^ mask[i % 4]
         if hasattr(unmasked, 'tobytes'):
-            # tostring was deprecated in py32.  It hasn't been removed,
-            # but since we turn on deprecation warnings in our tests
-            # we need to use the right one.
             return unmasked.tobytes()
         else:
             return unmasked.tostring()
 
-    def _read_strict(self, bufsize):
+    def _read_strict(self, bufsize: int) -> bytes:
         remaining = bufsize
         _bytes = b""
         while remaining:
@@ -65,10 +105,24 @@ class SocketPool():
             remaining = bufsize - len(_bytes)
         return _bytes
 
-    def read_frame(self):
-        """
-        recieve data as frame from server.
-        """
+    def check_syntax(self, msg: bytes) -> bool:
+        if msg == b'\x03\xe9':
+            raise ShortMsgError('%s 断连' % self.ip)
+
+        code, msg = wordsFilterTool.deal(msg)
+        if code:
+            return True
+
+        self._write_frame(msg)
+
+    def send(self, words: bytes) -> None:
+        self._write_frame(words)
+
+    def read(self) -> bytes:
+        _, _, rec = self._read_frame()
+        return rec
+
+    def _read_frame(self) -> tuple:
         header_bytes = self._read_strict(2)
         b1 = header_bytes[0] if six.PY3 else ord(header_bytes[0])
         fin = b1 >> 7 & 1
@@ -89,13 +143,10 @@ class SocketPool():
             mask_key = self._read_strict(4)
         data = self._read_strict(length)
         if mask:
-            data = self.mask_or_unmask(mask_key, data)
+            data = self._mask_or_unmask(mask_key, data)
         return fin, opcode, data
 
     def can_read(self, timeout=0.0):
-        '''
-        Return ``True`` if new data can be read from the socket.
-        '''
         r, w, e = [self.sock], [], []
         try:
             r, w, e = select.select(r, w, e, timeout)
@@ -104,3 +155,32 @@ class SocketPool():
                 return False
             self.sock.close()
         return self.sock in r
+
+    def _write_frame(self, data: bytes, fin=True, opcode=0x1) -> None:
+        mask_outgoing = False
+        if fin:
+            finbit = 0x80
+        else:
+            finbit = 0
+        frame = struct.pack("B", finbit | opcode)
+        l = len(data)
+        if mask_outgoing:
+            mask_bit = 0x80
+        else:
+            mask_bit = 0
+        if l < 126:
+            frame += struct.pack("B", l | mask_bit)
+        elif l <= 0xFFFF:
+            frame += struct.pack("!BH", 126 | mask_bit, l)
+        else:
+            frame += struct.pack("!BQ", 127 | mask_bit, l)
+        if mask_outgoing:
+            mask = os.urandom(4)
+            data = mask + self._mask_or_unmask(mask, data)
+        if isinstance(data, six.text_type):
+            data = data.encode('utf-8')
+        frame += data
+        try:
+            self.sock.sendall(frame)
+        except socket.error:
+            self.sock.close()
--- app_chatroom/views.py
+++ app_chatroom/views.py
@@ -1,24 +1,12 @@
-import base64
-import hashlib
 import json
-import os
-import struct
-import time
-import random
-import socket
-import array
+import traceback
 
 from collections import deque
 
-import six
-from django.http import HttpResponse
-from django.shortcuts import render
 # Create your views here.
-from dwebsocket.decorators import require_websocket, accept_websocket
 from libs import myLog
 from TIE.settings import WordsQueueConf
-from app_chatroom.models import ChatUser, SocketPool
-from tools.thesaurus import wordsFilterTool
+from app_chatroom.models import ChatUser, ShortMsgError
 
 # TODO:多个聊天室后期实现，大概会做一个初次信息判定，初次判定时 用户资料（昵称和ip）会被整合至request请求
 sessionSet = set()
@@ -28,174 +16,62 @@ def _all_user_send(m: (str, bytes), q: set) -> None:
     '''m:消息;q:用户池'''
     if not m or not q: return
 
+    if isinstance(m, dict):
+        m = json.dumps(m)
+
     if isinstance(m, str):
         m = m.encode()
 
     for i in q:
-        i.websocket.send(m)
+        i.send(m)
 
 # 加入
-def _join(request: object) -> None:
-    msg = '%s 加入' % request
+def _join(obj: ChatUser) -> None:
+    msg = '%s 加入' % obj.ip
     myLog.debug(msg)
+    msg = {"message": msg, "type": "system"}
+
     _all_user_send(msg, sessionSet)
-    sessionSet.add(request)
 
 # 发言
-def _speak(msg: str, request: object) -> None:
+def _speak(msg: (str, bytes)) -> None:
     myLog.debug(msg)
-    if msg:
-        code, words = wordsFilterTool.deal(msg, request)
-        if code: _all_user_send(msg, sessionSet)
+
+    if isinstance(msg, bytes):
+        msg = msg.decode()
+    msg = json.loads(msg)
+    msg['type'] = 'usermsg'
+
+    _all_user_send(json.dumps(msg), sessionSet)
 
 # 离开
-def _leave(request: object) -> None:
-    sessionSet.remove(request)
-    msg = '%s 离开' % request
+def _leave(obj: ChatUser) -> None:
+    msg = '%s 离开' % obj.ip
     myLog.debug(msg)
+
+    msg = {"message": msg, "type": "system"}
     _all_user_send(msg, sessionSet)
 
-
-def _get_wsgi_sock(request):
-    if 'gunicorn.socket' in request.META:
-        sock = request.META['gunicorn.socket']
-    else:
-        wsgi_input = request.META['wsgi.input']
-        if hasattr(wsgi_input, '_sock'):
-            sock = wsgi_input._sock
-        elif hasattr(wsgi_input, 'rfile'):  # gevent
-            if hasattr(wsgi_input.rfile, '_sock'):
-                sock = wsgi_input.rfile._sock
-            else:
-                sock = wsgi_input.rfile.raw._sock
-        elif hasattr(wsgi_input, 'raw'):
-            sock = wsgi_input.raw._sock
-        elif hasattr(wsgi_input, 'stream') and hasattr(wsgi_input.stream, 'raw'):
-            sock = wsgi_input.stream.raw._sock
-        else:
-            raise ValueError('Socket not found in wsgi.input')
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    return sock
-
-l = []
-random.choice('abcdefghijklmnopqrstuvwxyz!@#$%^&*()')
-
-accept_header = (
-            "HTTP/1.1 101 Switching Protocols\r\n"
-            "Upgrade: websocket\r\n"
-            "Connection: Upgrade\r\n"
-            "Sec-WebSocket-Accept: %s\r\n"
-            "\r\n"
-            )
-
-
-def compute_accept_value(key):
-    sha1 = hashlib.sha1()
-    sha1.update(key.encode())
-    sha1.update(b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11")  # Magic value
-    return base64.b64encode(sha1.digest())
-
-def mask_or_unmask(mask, data):
-    mask = array.array("B", mask)
-    unmasked = array.array("B", data)
-    for i in range(len(data)):
-        unmasked[i] = unmasked[i] ^ mask[i % 4]
-    if hasattr(unmasked, 'tobytes'):
-        return unmasked.tobytes()
-    else:
-        return unmasked.tostring()
-def _write_frame(sock, data, fin=True, opcode=0x1):
-    mask_outgoing = False
-    if fin:
-        finbit = 0x80
-    else:
-        finbit = 0
-    frame = struct.pack("B", finbit | opcode)
-    l = len(data)
-    if mask_outgoing:
-        mask_bit = 0x80
-    else:
-        mask_bit = 0
-    if l < 126:
-        frame += struct.pack("B", l | mask_bit)
-    elif l <= 0xFFFF:
-        frame += struct.pack("!BH", 126 | mask_bit, l)
-    else:
-        frame += struct.pack("!BQ", 127 | mask_bit, l)
-    if mask_outgoing:
-        mask = os.urandom(4)
-        data = mask + mask_or_unmask(mask, data)
-    if isinstance(data, six.text_type):
-        data = data.encode('utf-8')
-    frame += data
-    try:
-        sock.sendall(frame)
-        print('frame', frame)
-    except socket.error:
-        sock.close()
-
-def _read_strict(sock, bufsize):
-    remaining = bufsize
-    _bytes = b""
-    while remaining:
-        _buffer = sock.recv(remaining)
-        if not _buffer:
-            raise socket.error(socket.EBADF, 'Bad file descriptor')
-        _bytes += _buffer
-        remaining = bufsize - len(_bytes)
-    return _bytes
-
-def read_frame(sock):
-    """
-    recieve data as frame from server.
-    """
-    header_bytes = _read_strict(sock, 2)
-    b1 = header_bytes[0] if six.PY3 else ord(header_bytes[0])
-    fin = b1 >> 7 & 1
-    opcode = b1 & 0xf
-    b2 = header_bytes[1] if six.PY3 else ord(header_bytes[1])
-    mask = b2 >> 7 & 1
-    length = b2 & 0x7f
-
-    length_data = ""
-    if length == 0x7e:
-        length_data = _read_strict(sock, 2)
-        length = struct.unpack("!H", length_data)[0]
-    elif length == 0x7f:
-        length_data = _read_strict(sock, 8)
-        length = struct.unpack("!Q", length_data)[0]
-    mask_key = ""
-    if mask:
-        mask_key = _read_strict(sock, 4)
-    data = _read_strict(sock, length)
-    if mask:
-        data = mask_or_unmask(mask_key, data)
-    return fin, opcode, data
-
-# @accept_websocket
-def cli_accept(request) -> HttpResponse:
+def cli_accept(request) -> None:
     '''客户端总处理函数'''
-    h = request.META['HTTP_SEC_WEBSOCKET_KEY']
-    cliSocket = _get_wsgi_sock(request)
-    l.append(cliSocket)
-    k = compute_accept_value(h)
-    s = accept_header % k.decode()
+    cliSocket = ChatUser(request)
+    # 加入
+    _join(cliSocket)
+    sessionSet.add(cliSocket)
 
+    try:
+        while not cliSocket.can_read():
+            msg = cliSocket.read()
+            # 校验合法
+            if cliSocket.check_syntax(msg):
+                _speak(msg)
 
-    if isinstance(s, str): s = s.encode()
-    print(s)
-    cliSocket.send(s)
+    except ShortMsgError:
+        # 离开
+        _leave(cliSocket)
+        sessionSet.remove(cliSocket)
 
-    for i in range(5):
-        pass
-        print('send')
-        _write_frame(cliSocket, b'{"message": "123", "type": "usermsg", "name": "123"}')
-    mySock = SocketPool(cliSocket)
-    while not mySock.can_read():
-        _, _, res = read_frame(cliSocket)
-        res = json.loads(res.decode())
-        res['type'] = 'usermsg'
-        res = json.dumps(res).encode()
-        for i in l:
-            print('遍历', i)
-            _write_frame(i, res)
+    except:
+        myLog.error(traceback.format_exc())
+        _leave(cliSocket)
+        sessionSet.remove(cliSocket)
